ef53cfaf93cc986bb8a3d315f6f4b2e4c91dd897
sbx_user1051 <sbx_user1051@169.254.84.173>
2024-06-01 15:27:22 +0000

    Sat Jun 01 15:27:22 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -51,12 +51,11 @@ public class CheckoutSolution {
         }
 
         while(counter[1] > 0){
-            while(counter[2] >= 2 || counter[1] > 0){
             if (counter[4] >= 2) {
                 counter[1]--;
                 totalPrices[4] = totalPrices[4] + 80;
                 counter[4] = counter[4] - 2;
-            }}
+            }
             if(counter[1] >= 2){
                 totalPrices[1] = totalPrices[1] + 45;
                 counter[1] = counter[1] - 2;
@@ -76,6 +75,7 @@ public class CheckoutSolution {
         return totalPrice;
     }
 }
+
 
 
 
