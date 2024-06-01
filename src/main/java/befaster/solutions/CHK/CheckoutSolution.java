6f8fad9de02423304ba2847112f1a73f0955df7b
sbx_user1051 <sbx_user1051@169.254.84.173>
2024-06-01 14:26:02 +0000

    Sat Jun 01 14:26:02 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,8 +6,12 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
         int totalPrice = 0;
         int counter = 0;
-        
+        for(int i = 0; i<skus.length(); i++)
+        {
+            if(Character.)
+        }
 
     }
 }
+
 
