c52bdf9f16a0a4b34ba07a4384a76343f5b10364
sbx_user1051 <sbx_user1051@169.254.84.173>
2024-06-01 14:53:12 +0000

    Sat Jun 01 14:53:12 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,45 +7,42 @@ public class CheckoutSolution {
         int totalPrice = 0;
         int[] counter = {0, 0, 0, 0};
         int[] totalPrices = {0, 0, 0, 0};
-        for(int i = 0; i<skus.length(); i++)
-        {
-            if(skus.isEmpty())
-            {
-                totalPrice = 0;
-                break;
-            }
-            if(skus.charAt(i)== 'A' || skus.charAt(i) == 'B' || skus.charAt(i) == 'C' || skus.charAt(i) == 'D')
-            {
-                switch (skus.charAt(i)) {
-                    case 'A':
-                        counter[0]++;
-                        break;
-                    case 'B':
-                        counter[1]++;
-                        break;
-                    case 'C':
-                        counter[2]++;
-                        break;
-                    case 'D':
-                        counter[3]++;
-                        break;
-                }
-            }
-            else {
-                return -1;
-            }
-        }
-        while(counter[0] != 0){
-        if(counter[0] >= 3) {
-            totalPrices[0] = totalPrices[0] + 130;
-            counter[0] = counter[0] - 3;
+        if(skus.isEmpty()) {
+            return totalPrice;
         }
         else {
-            totalPrices[0] = counter[0] * 50;
-            counter[0] = 0;
+            for (int i = 0; i < skus.length(); i++) {
+                if (skus.charAt(i) == 'A' || skus.charAt(i) == 'B' || skus.charAt(i) == 'C' || skus.charAt(i) == 'D') {
+                    switch (skus.charAt(i)) {
+                        case 'A':
+                            counter[0]++;
+                            break;
+                        case 'B':
+                            counter[1]++;
+                            break;
+                        case 'C':
+                            counter[2]++;
+                            break;
+                        case 'D':
+                            counter[3]++;
+                            break;
+                    }
+                } else {
+                    return -1;
+                }
+            }
         }
+        while(counter[0] > 0){
+            if(counter[0] >= 3) {
+                totalPrices[0] = totalPrices[0] + 130;
+                counter[0] = counter[0] - 3;
+            }
+            else {
+                totalPrices[0] = counter[0] * 50;
+                counter[0] = 0;
+            }
         }
-        while(counter[1] != 0){
+        while(counter[1] > 0){
             if(counter[1] >= 2){
                 totalPrices[1] = totalPrices[1] + 45;
                 counter[1] = counter[1] - 2;
@@ -64,3 +61,4 @@ public class CheckoutSolution {
         return totalPrice;
     }
 }
+
