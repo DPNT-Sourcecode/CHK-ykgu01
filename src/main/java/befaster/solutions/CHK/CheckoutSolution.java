939ac84f8fa833c52ded465efde430ddf1171f19
sbx_user1051 <sbx_user1051@169.254.84.173>
2024-06-01 13:43:28 +0000

    Sat Jun 01 13:43:28 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -9,6 +9,7 @@ public class CheckoutSolution {
         String sku = "";
         int totalPrice = 0;
         for(int i = 0; i<skus.length(); i++) {
+            if(skus.charAt(i) != 'A' || skus.charAt(i) != 'B' || skus.charAt(i) != 'C' || skus.charAt(i) != 'D' || Character.isDigit(skus.charAt(i))) {}
             if(Character.isLetter(skus.charAt(i))) {
                 switch(skus.charAt(i)) {
                     case 'A':
@@ -43,12 +44,10 @@ public class CheckoutSolution {
                 productletter += skus.charAt(i);
                 product = Integer.parseInt(productletter);
             }
-            else{
-                return -1;
-            }
         }
         return totalPrice;
     }
 }
+
 
 
