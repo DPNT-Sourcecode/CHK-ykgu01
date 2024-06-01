10659e0fd050044165ec12196e525ea4da36cc88
sbx_user1051 <sbx_user1051@169.254.84.173>
2024-06-01 15:58:37 +0000

    Sat Jun 01 15:58:37 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,8 +5,8 @@ import befaster.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         int totalPrice = 0;
-        int[] counter = {0, 0, 0, 0, 0, 0};
-        int[] totalPrices = {0, 0, 0, 0, 0, 0};
+        int[] counter = new int[26];
+        int[] totalPrices = new int[26];
         if(skus.isEmpty()) {
             return totalPrice;
         }
@@ -36,6 +36,63 @@ public class CheckoutSolution {
                             counter[6]++;
                             break;
                         case 'H':
+                            counter[7]++;
+                            break;
+                        case 'I':
+                            counter[8]++;
+                            break;
+                        case 'J':
+                            counter[9]++;
+                            break;
+                        case 'K':
+                            counter[10]++;
+                            break;
+                        case 'L':
+                            counter[11]++;
+                            break;
+                        case 'M':
+                            counter[12]++;
+                            break;
+                        case 'N':
+                            counter[13]++;
+                            break;
+                        case 'O':
+                            counter[14]++;
+                            break;
+                        case 'P':
+                            counter[15]++;
+                            break;
+                        case 'Q':
+                            counter[16]++;
+                            break;
+                        case 'R':
+                            counter[17]++;
+                            break;
+                        case 'S':
+                            counter[18]++;
+                            break;
+                        case 'T':
+                            counter[19]++;
+                            break;
+                        case 'U':
+                            counter[20]++;
+                            break;
+                        case 'V':
+                            counter[21]++;
+                            break;
+                        case 'W':
+                            counter[22]++;
+                            break;
+                        case 'X':
+                            counter[23]++;
+                            break;
+                        case 'Y':
+                            counter[24]++;
+                            break;
+                        case 'Z':
+                            counter[25]++;
+                            break;
+
                     }
                 } else {
                     return -1;
@@ -93,4 +150,5 @@ public class CheckoutSolution {
         return totalPrice;
     }
 }
+
 
